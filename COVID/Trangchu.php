762a3f4ad239de3ca69ae52e94d1458729a421e6
Thành Đạt <tthanhdat1412@gmail.com>
2022-12-26 21:05:34 +0700

    first commit

---
+++ COVID/Trangchu.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Trang Chủ</title>
+<link rel="stylesheet" type="text/css" href="CSS/style.css"/>
+</head>
+
+<body>
+	<div class="container">
+	<div class="header">
+		<div class="top-header">
+			<div id="logan">HÃY GIỮ AN TOÀN CHO BẠN, GIA ĐÌNH VÀ CỘNG ĐỒNG TRƯỚC ĐẠI DỊCH COVID-19</div>
+		</div>
+		<div class="content">
+			<div id="logo"><img src="IMG/image 1.png" height="80"/></div>
+			<div id="title">ChiEmCovid</div>
+			<div id="logo2"><img src="IMG/image 2.png"height="110"/></div>
+			<div id="contact"><b style="padding-left: 50px;">HOTLINE</b>: 19006868 <br> <b>Email</b>: chiemcaykhe@gmail.com</div>
+			<div id="sig">
+				<a href="#" style="padding-right: 15px;text-decoration:none;color:#000000;">Đăng nhập</a>
+				<a href="#" style="text-decoration: none;color:#000000;">Đăng ký</a>
+			</div>
+		</div>
+	</div>
+		<div class="nav">
+			<ul>
+				<li>Trang chủ</li>
+				<li>Giới thiệu</li>
+				<li>Khai báo y tế</li>
+				<li>Điều trị</li>
+				<li>Hướng dẫn</li>
+				<li>Tư vấn - Hỗ trợ</li>
+				<li>Tài khoản</li>
+			</ul>
+		</div>
+		<div class="session">
+			<div id="ar_left">
+				<img src="IMG/image 3.png" width="100%"; height="600px"/>
+			</div>
+			<div id="ar_right">
+				<img src="IMG/image 4.png"width="100%"; height="600px"/>
+			</div>
+		</div>
+		<div class="footer">
+			<div id="ft_contact"><b style="padding-left: 50px;">HOTLINE</b>: 19006868 <br> <b>Email</b>: chiemcaykhe@gmail.com</div>
+			<div class="ft_nav">
+				<ul>
+					<li>Hướng dẫn</li>
+					<li>Tư vấn - Hỗ trợ</li>
+					<li>Điều khoản sử dụng</li>
+				</ul>
+			</div>
+			</div>
+			
+	</div>
+</body>
+</html>
